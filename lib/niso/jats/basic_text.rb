e3b1a0345d443358b98182ad7732ab3160a742ce
Valerii Pohrebniak <valerii.pohrebniak@rubygarage.org>
2024-10-24 00:29:58 +0300

    Fix autoload with zeitwerk

--- lib/niso/jats/basic_text.rb
+++ lib/niso/jats/basic_text.rb
@@ -1,45 +1,49 @@
-
 module Niso
   module Jats
     module BasicText
-      def self.included(base)
-        base.attribute :email, Email, collection: true
-        base.attribute :ext_link, ExtLink, collection: true
-        base.attribute :uri, Uri, collection: true
-        base.attribute :inline_supplementary_material, InlineSupplementaryMaterial, collection: true
-        base.attribute :related_article, RelatedArticle, collection: true
-        base.attribute :related_object, RelatedObject, collection: true
-        base.attribute :bold, Bold, collection: true
-        base.attribute :fixed_case, FixedCase, collection: true
-        base.attribute :italic, Italic, collection: true
-        base.attribute :monospace, Monospace, collection: true
-        base.attribute :overline, Overline, collection: true
-        base.attribute :roman, Roman, collection: true
-        base.attribute :sans_serif, SansSerif, collection: true
-        base.attribute :sc, Sc, collection: true
-        base.attribute :strike, Strike, collection: true
-        base.attribute :underline, Underline, collection: true
-        base.attribute :ruby, Ruby, collection: true
-        base.attribute :alternatives, Alternatives, collection: true
-        base.attribute :inline_graphic, InlineGraphic, collection: true
-        base.attribute :inline_media, InlineMedia, collection: true
-        base.attribute :private_char, PrivateChar, collection: true
-        base.attribute :chem_struct, ChemStruct, collection: true
-        base.attribute :inline_formula, InlineFormula, collection: true
-        base.attribute :tex_math, TexMath, collection: true
-        base.attribute :mml_math, :string, collection: true
-        base.attribute :abbrev, Abbrev, collection: true
-        base.attribute :index_term, IndexTerm, collection: true
-        base.attribute :index_term_range_end, IndexTermRangeEnd, collection: true
-        base.attribute :milestone_end, MilestoneEnd, collection: true
-        base.attribute :milestone_start, MilestoneStart, collection: true
-        base.attribute :named_content, NamedContent, collection: true
-        base.attribute :styled_content, StyledContent, collection: true
-        base.attribute :fn, Fn, collection: true
-        base.attribute :target, Target, collection: true
-        base.attribute :xref, Xref, collection: true
-        base.attribute :sub, Sub, collection: true
-        base.attribute :sup, Sup, collection: true
+      require_relative "target"
+
+      def self.included(base) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+        base.class_eval do
+          attribute :email, Email, collection: true
+          attribute :ext_link, ExtLink, collection: true
+          attribute :uri, Uri, collection: true
+          attribute :inline_supplementary_material,
+                    InlineSupplementaryMaterial, collection: true
+          attribute :related_article, RelatedArticle, collection: true
+          attribute :related_object, RelatedObject, collection: true
+          attribute :bold, Bold, collection: true
+          attribute :fixed_case, FixedCase, collection: true
+          attribute :italic, Italic, collection: true
+          attribute :monospace, Monospace, collection: true
+          attribute :overline, Overline, collection: true
+          attribute :roman, Roman, collection: true
+          attribute :sans_serif, SansSerif, collection: true
+          attribute :sc, Sc, collection: true
+          attribute :strike, Strike, collection: true
+          attribute :underline, Underline, collection: true
+          attribute :ruby, Ruby, collection: true
+          attribute :alternatives, Alternatives, collection: true
+          attribute :inline_graphic, InlineGraphic, collection: true
+          attribute :inline_media, InlineMedia, collection: true
+          attribute :private_char, PrivateChar, collection: true
+          attribute :chem_struct, ChemStruct, collection: true
+          attribute :inline_formula, InlineFormula, collection: true
+          attribute :tex_math, TexMath, collection: true
+          attribute :mml_math, :string, collection: true
+          attribute :abbrev, Abbrev, collection: true
+          attribute :index_term, IndexTerm, collection: true
+          attribute :index_term_range_end, IndexTermRangeEnd, collection: true
+          attribute :milestone_end, MilestoneEnd, collection: true
+          attribute :milestone_start, MilestoneStart, collection: true
+          attribute :named_content, NamedContent, collection: true
+          attribute :styled_content, StyledContent, collection: true
+          attribute :fn, Fn, collection: true
+          attribute :target, Target, collection: true
+          attribute :xref, Xref, collection: true
+          attribute :sub, Sub, collection: true
+          attribute :sup, Sup, collection: true
+        end
       end
     end
   end
