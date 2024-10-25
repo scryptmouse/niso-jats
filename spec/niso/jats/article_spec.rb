49792e5e9b3cae1967e191c73d349905fe2a4020
River He <novtopro@gmail.com>
2024-10-25 14:00:58 +0800

    ISSUE#1 Skip test

--- spec/niso/jats/article_spec.rb
+++ spec/niso/jats/article_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 RSpec.describe Niso::Jats::Article do
-  it "round-trips bmj_sample.xml with equivalent-xml" do
+  xit "round-trips bmj_sample.xml with equivalent-xml" do
     input = fixture("bmj_sample.xml").read
 
     parsed = described_class.from_xml(input)
