95cc8d07634e2b55a25eb576fdb6fd42b5b096d6
Ronald Tse <ronald.tse@ribose.com>
2024-10-27 21:06:39 +0800

    chore: add xml-c14n comparison for more accurate pinpointing in tests

--- spec/niso/jats/article_spec.rb
+++ spec/niso/jats/article_spec.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
 RSpec.describe Niso::Jats::Article do
-  it "round-trips bmj_sample.xml with equivalent-xml" do
-    input = fixture("bmj_sample.xml").read
+  def file_contents(filename)
+    File.read(filename)
+  end
 
-    parsed = described_class.from_xml(input)
-    output = parsed.to_xml(pretty: true, declaration: true, encoding: "utf-8")
+  glob_path = Pathname.new(__dir__)
+                      .join("../../fixtures/*.xml")
 
-    expect(output).to be_equivalent_to(input)
+  Dir.glob(glob_path)[0..1].each do |filename|
+    fn = Pathname.new(filename).basename
+    it "round-trips #{fn} with equivalent-xml" do
+      input = file_contents(Pathname.new(filename))
+      parsed = described_class.from_xml(input)
+      generated = parsed.to_xml(
+        pretty: true,
+        declaration: true,
+        encoding: "utf-8",
+      )
+
+      expect(generated).to be_equivalent_to(input)
+    end
+
+    it "round-trips #{fn} with xml-c14" do
+      input = file_contents(Pathname.new(filename))
+      parsed = described_class.from_xml(input)
+      generated = parsed.to_xml(
+        pretty: true,
+        declaration: true,
+        encoding: "utf-8",
+      )
+
+      expect(generated).to be_analogous_with(input)
+    end
   end
 end
