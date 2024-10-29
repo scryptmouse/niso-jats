f57567357bacc5ecd8a67124d44255d5ef2cd53b
Ronald Tse <ronald.tse@ribose.com>
2024-10-30 00:27:31 +0800

    chore: add test XML files from official niso-jats repo

--- spec/niso/jats/article_spec.rb
+++ spec/niso/jats/article_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Niso::Jats::Article do
     let(:fixture) { file_fixture("pnas_sample.xml") }
     it_behaves_like "a serializer"
   end
+
+  Dir[File.join(__dir__, "../../fixtures/niso-jats/**/Smallsamples/*.xml")].each do |file|
+    # Skip files from draft versions
+    next if file.include?("/0.4/")
+    filename = Pathname.new(file)
+    context "with #{filename.relative_path_from(File.join(__dir__, "../.."))}" do
+      let(:fixture) { File.new(file) }
+      it_behaves_like "a serializer"
+    end
+  end
 end
