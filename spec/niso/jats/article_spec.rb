3ecc504226de9245bb24cf7570ad0646b59a0526
Ronald Tse <ronald.tse@ribose.com>
2024-11-02 16:13:26 +0800

    fix: skip version 1.1d3 tests and group versions, fixes #16, fixes #20

--- spec/niso/jats/article_spec.rb
+++ spec/niso/jats/article_spec.rb
@@ -12,12 +12,21 @@ RSpec.describe Niso::Jats::Article do
   end
 
   Dir[File.join(__dir__, "../../fixtures/niso-jats/**/Smallsamples/*.xml")].each do |file|
-    # Skip files from draft versions
-    next if file.include?("/0.4/")
-    filename = Pathname.new(file)
-    context "with #{filename.relative_path_from(File.join(__dir__, "../.."))}" do
-      let(:fixture) { File.new(file) }
-      it_behaves_like "a serializer"
+    version = file.match(/\/(?<version>\d+\.\d\w?\d?)\/Smallsamples/)[:version]
+
+    # Skip files from old draft version
+    next if version == "0.4"
+
+    # All 1.1d3 files contain a bad xmlns for ali that is missing the trailing slash
+    # xmlns:ali="http://www.niso.org/schemas/ali/1.0"
+    next if version == "1.1d3"
+
+    describe "Smallsamples from NISO JATS #{version}" do
+      filename = Pathname.new(file)
+      context "with #{filename.relative_path_from(File.join(__dir__, "../../fixtures"))}" do
+        let(:fixture) { File.new(file) }
+        it_behaves_like "a serializer"
+      end
     end
   end
 end
